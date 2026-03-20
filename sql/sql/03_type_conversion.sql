ed46e78f352eb1353ff6504e20033df36d6abd22
Vlads <87931410+Vladislayyyyyy@users.noreply.github.com>
2026-03-20 15:58:45 +0300

    Update 03_type_conversion.sql

--- sql/sql/03_type_conversion.sql
+++ sql/sql/03_type_conversion.sql
@@ -10,3 +10,6 @@ WHERE "Price Each" !~ '^-?[0-9]+\.?[0-9]*$';
 DELETE FROM all_sales_2019
 WHERE "Quantity Ordered" = 'Quantity Ordered'
    OR "Price Each" = 'Price Each';
+
+ALTER TABLE all_sales_2019 ADD COLUMN IF NOT EXISTS quantity_int INTEGER;
+ALTER TABLE all_sales_2019 ADD COLUMN IF NOT EXISTS price_numeric NUMERIC(10,2);
